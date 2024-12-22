9505be87d484f8548b23463ee8f28ff0e818c4a5
Piyush Choudhari <choudhari.piyush@gmail.com>
2024-12-22 04:07:12 +0000

    added draw_board and check_winner

--- main.py
+++ main.py
@@ -27,3 +27,69 @@ class TicTacToe:
         self.game_board = np.zeros(
             (self.board_size, self.board_size, 3), dtype=np.uint8
         )
+
+    def get_cell_from_coordinates(self, x, y):
+        row = int(y * 3 // self.board_size)
+        col = int(x * 3 // self.board_size)
+        return row, col
+
+    def draw_board(self):
+        self.game_board.fill(255)
+
+        for i in range(1, 3):
+            cv2.line(
+                self.game_board,
+                (i * self.cell_size, 0),
+                (i * self.cell_size, self.board_size),
+                (0, 0, 0),
+                2,
+            )
+            cv2.line(
+                self.game_board,
+                (0, i * self.cell_size),
+                (self.board_size, i * self.cell_size),
+                (0, 0, 0),
+                2,
+            )
+
+        for i in range(3):
+            for j in range(3):
+                center = (
+                    j * self.cell_size + self.cell_size // 2,
+                    i * self.cell_size + self.cell_size // 2,
+                )
+
+                if self.board[i, j] == 1:
+                    cv2.line(
+                        self.game_board,
+                        (center[0] - 60, center[1] - 60),
+                        (center[0] + 60, center[1] + 60),
+                        (255, 0, 0),
+                        3,
+                    )
+                    cv2.line(
+                        self.game_board,
+                        (center[0] + 60, center[1] - 60),
+                        (center[0] - 60, center[1] + 60),
+                        (255, 0, 0),
+                        3,
+                    )
+                elif self.board[i, j] == -1:
+                    cv2.circle(self.game_board, center, 60, (0, 0, 255), 3)
+
+    def check_winner(self):
+        for i in range(3):
+            if abs(sum(self.board[i, :])) == 3:
+                return self.board[i, 0]
+            if abs(sum(self.board[:, i])) == 3:
+                return self.board[0, i]
+
+        if abs(sum(np.diag(self.board))) == 3:
+            return self.board[0, 0]
+        if abs(sum(np.diag(np.fliplr(self.board)))) == 3:
+            return self.board[0, 2]
+
+        if np.count_nonzero(self.board) == 9:
+            return 0
+
+        return None
