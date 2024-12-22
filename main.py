543b9a4f266240ae1759d27a4d979166e38a19c4
Piyush Choudhari <choudhari.piyush@gmail.com>
2024-12-22 03:59:33 +0000

    tic tac toe contructor

--- main.py
+++ main.py
@@ -3,3 +3,27 @@ import numpy as np
 import mediapipe as mp
 import random
 import math
+
+
+class TicTacToe:
+    def __init__(self):
+        self.board = np.zeros((3, 3), dtype=int)
+        self.human_player = 1
+        self.computer_player = -1
+        self.game_over = False
+        self.winner = None
+
+        self.mp_hands = mp.solutions.hands
+        self.hands = self.mp_hands.Hands(
+            static_image_mode=False,
+            max_num_hands=1,
+            min_detection_confidence=0.7,
+            min_tracking_confidence=0.7,
+        )
+        self.mp_draw = mp.solutions.drawing_utils
+
+        self.cell_size = 200
+        self.board_size = self.cell_size * 3
+        self.game_board = np.zeros(
+            (self.board_size, self.board_size, 3), dtype=np.uint8
+        )
